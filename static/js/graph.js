f611b1f45bf9d64716f3f8fa38c5e8a5812693b8
Andrew Stead <andrew@andrewstead.co.uk>
2018-01-20 12:04:42 +0000

    Added winning country and margin pie charts to Champions page

--- static/js/graph.js
+++ static/js/graph.js
@@ -25,6 +25,12 @@ function makeGraphs(error, crucible_results) {
     var finalWinner = finals.dimension(function (d) {
     	return d["winner"];
     });
+    var winnerNat = finals.dimension(function (d) {
+    	return d["winner_nat"];
+    });
+    var winnerGap = finals.dimension(function (d) {
+    	return (d["winner_score"] - d["loser_score"]);
+    });
     var finalLoser = finals.dimension(function (d) {
     	return d["loser"];
     });
@@ -44,19 +50,24 @@ function makeGraphs(error, crucible_results) {
     	return d["year"];
     });
 
-    // Filtering the data
+    // Filtering the data for finals only
     var finalFilter = finalRound.filter("The Final");
+
+    // Setting color scales for pie charts
+    var blockSlices = d3.scale.ordinal().range(["#000000", "#ff6666", "#0000ff", "#663300", "#006600", "#ffff00", "#ee0000", "#ffffff"]);
+    var shadeSlices = d3.scale.ordinal().range(["rgb(100,50,0)", "rgb(105,56,7)", "rgb(110,62,14)", "rgb(115,68,21)", "rgb(120,74,28)", "rgb(125,80,35)", "rgb(130,86,42)", "rgb(135,92,49)", "rgb(140,98,56)", "rgb(145,104,63)", "rgb(150,110,70)", "rgb(155,116,77)", "rgb(160,122,84)", "rgb(165,128,91)", "rgb(170,134,98)", "rgb(175,140,105)", "rgb(180,146,112)", "rgb(185,152,119)", "rgb(190,158,126)", "rgb(195,164,133)", "rgb(200,170,140"]);
 
     // Grouping the data
     var allFinals = finals.groupAll();
     var champions = finalWinner.group();
+    var winningCountry = winnerNat.group();
+    var winningMargin = winnerGap.group();
     var runnersUp = finalLoser.group();
     var yearGroup = allYears.group();
     var roundGroup = allRounds.group();
     var allMatches = allResults.groupAll();
     var matchWinners = allWinners.group();
     var matchLosers = allLosers.group();
-
     var allFrames = allResults.groupAll().reduceSum(
     		function (d) {
     			return (d["winner_score"] + d["loser_score"]);
@@ -66,6 +77,8 @@ function makeGraphs(error, crucible_results) {
     // Charts
     var tournaments = dc.numberDisplay("#totalTournaments");
     var winnerList = dc.rowChart("#winnersRow");
+    var winnersByCountry = dc.pieChart("#winningCountry");
+    var winnersByMargin = dc.pieChart("#winningMargin");
     var runnerUpList = dc.rowChart("#runnerUpRow");
     var finalResults = dc.dataTable("#winnersTable");
     var yearSelection = dc.selectMenu('#yearSelect');
@@ -88,10 +101,10 @@ function makeGraphs(error, crucible_results) {
     	.dimension(finalWinner)
     	.group(champions)
     	.width(250)
-    	.height(300)
+    	.height(250)
     	.rowsCap(6)
     	.ordering(function(d) { return -d.value; })
-    	.xAxis().ticks(7);
+    	.elasticX(true);
 
     runnerUpList
     	.ordinalColors(["#996600"])
@@ -101,7 +114,24 @@ function makeGraphs(error, crucible_results) {
     	.height(300)
     	.rowsCap(11)
     	.ordering(function(d) { return -d.value; })
-    	.xAxis().ticks(7);
+    	.elasticX(true);
+
+    winnersByCountry
+    	.height(200)
+    	.radius(100)
+    	.innerRadius(20)
+    	.dimension(winnerNat)
+    	.group(winningCountry)
+    	.ordering(function(d) { return -d.value; })
+    	.colors(blockSlices);
+
+    winnersByMargin
+    	.height(200)
+    	.radius(100)
+    	.innerRadius(20)
+    	.dimension(winnerGap)
+    	.group(winningMargin)
+    	.colors(shadeSlices);
 
     finalResults
     	.dimension(finalYear)
@@ -200,8 +230,7 @@ function makeGraphs(error, crucible_results) {
     	.height(250)
     	.rowsCap(10)
     	.ordering(function(d) { return -d.value; })
-    	.elasticX(true)
-    	.xAxis().ticks(10);
+    	.elasticX(true);
 
     losingPlayers
     	.ordinalColors(["#996600"])
@@ -212,8 +241,7 @@ function makeGraphs(error, crucible_results) {
     	.height(250)
     	.rowsCap(10)
     	.ordering(function(d) { return -d.value; })
-    	.elasticX(true)
-    	.xAxis().ticks(10);
+    	.elasticX(true);
 
     dc.renderAll();
 }
