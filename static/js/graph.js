f872c59b35af7caca595adead6f018f501383fdb
Andrew Stead <andrew@andrewstead.co.uk>
2018-01-22 00:19:18 +0000

    Added Win/Loss and Opponent charts to Players page

--- static/js/graph.js
+++ static/js/graph.js
@@ -63,6 +63,12 @@ function makeGraphs(error, crucible_results) {
     var playerList = playerResults.dimension(function (d) {
     	return d["player_1"];
     });
+    var opponentsList = playerResults.dimension(function (d) {
+    	return d["player_2"];
+    });
+    var playerOutcomes = playerResults.dimension(function (d) {
+        return d["player_1_result"];
+    });
 
     // Filtering the data for finals only
     var finalFilter = finalRound.filter("The Final");
@@ -70,6 +76,7 @@ function makeGraphs(error, crucible_results) {
     var duplicateFilter = allOutcomes.filter("W");
 
     // Setting color scales for pie charts
+    var booleanSlices = d3.scale.ordinal().range(["#ee0000", "#000000"]);
     var blockSlices = d3.scale.ordinal().range(["#000000", "#ff6666", "#0000ff", "#663300", "#006600", "#ffff00", "#ee0000", "#ffffff"]);
     var shadeSlices = d3.scale.ordinal().range(["rgb(100,50,0)", "rgb(105,56,7)", "rgb(110,62,14)", "rgb(115,68,21)", "rgb(120,74,28)", "rgb(125,80,35)", "rgb(130,86,42)", "rgb(135,92,49)", "rgb(140,98,56)", "rgb(145,104,63)", "rgb(150,110,70)", "rgb(155,116,77)", "rgb(160,122,84)", "rgb(165,128,91)", "rgb(170,134,98)", "rgb(175,140,105)", "rgb(180,146,112)", "rgb(185,152,119)", "rgb(190,158,126)", "rgb(195,164,133)", "rgb(200,170,140"]);
 
@@ -85,6 +92,8 @@ function makeGraphs(error, crucible_results) {
     var matchWinners = allWinners.group();
     var matchLosers = allLosers.group();
     var selectedPlayer = playerList.group();
+    var playerWinLoss = playerOutcomes.group();
+    var playerOpponent = opponentsList.group();
 
     // Grouping the data - sum
     var allFrames = allResults.groupAll().reduceSum(
@@ -109,6 +118,8 @@ function makeGraphs(error, crucible_results) {
     var losingPlayers = dc.rowChart("#mostDefeats");
     var playerSelection = dc.selectMenu('#playerSelect');
     var playerResults = dc.dataTable("#playerResults");
+    var winLossRecord = dc.pieChart("#winLoss");
+    var playerOpponents = dc.rowChart("#playerOpponents");
 
     tournaments
     	.formatNumber(d3.format("d"))
@@ -303,6 +314,25 @@ function makeGraphs(error, crucible_results) {
        			return d.player_2_score;
    			}
    		])
+
+    winLossRecord
+    	.dimension(playerOutcomes)
+    	.group(playerWinLoss)
+    	.height(200)
+    	.radius(100)
+    	.innerRadius(20)
+    	.colors(booleanSlices);
+
+    playerOpponents
+    	.ordinalColors(["#996600"])
+    	.dimension(opponentsList)
+    	.group(playerOpponent)
+    	.width(250)
+    	.height(300)
+    	.rowsCap(10)
+    	.othersGrouper(false)
+    	.ordering(function(d) { return -d.value; })
+    	.elasticX(true);
 
     dc.renderAll();
 }
