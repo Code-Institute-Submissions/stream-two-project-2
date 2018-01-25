95b230f5ba006527c97a8947a8e6014cc74c70f3
Andrew Stead <andrew@andrewstead.co.uk>
2018-01-25 17:25:16 +0000

    Added country and best performance charts to Careers page

--- static/js/careers.js
+++ static/js/careers.js
@@ -21,18 +21,35 @@ function makeGraphs(error, crucible_results) {
     var playerCareer = ndx.dimension(function (d) {
         return d["record_type"];
     });
+    var firstAppearance = ndx.dimension(function (d) {
+        return d["first"];
+    });
+    var careerNationality = ndx.dimension(function (d) {
+        return d["country"];
+    });
+    var bestRecord = ndx.dimension(function (d) {
+        return d["best_performance"];
+    });
 
     // Formatting numbers for data tables
     var twoDec = d3.format(".2f");
 
     // Setting color scales for pie charts
+    var blockSlices = d3.scale.ordinal().range(["#000000", "#ff6666", "#0000ff", "#663300", "#006600", "#ffff00", "#ee0000"]);
+    var shadeSlices = d3.scale.ordinal().range(["rgb(100,50,0)", "rgb(105,56,7)", "rgb(110,62,14)", "rgb(115,68,21)", "rgb(120,74,28)", "rgb(125,80,35)", "rgb(130,86,42)", "rgb(135,92,49)", "rgb(140,98,56)", "rgb(145,104,63)", "rgb(150,110,70)", "rgb(155,116,77)", "rgb(160,122,84)", "rgb(165,128,91)", "rgb(170,134,98)", "rgb(175,140,105)", "rgb(180,146,112)", "rgb(185,152,119)", "rgb(190,158,126)", "rgb(195,164,133)", "rgb(200,170,140"]);
 
     // Grouping the data - count
+    var crucibleDebut = firstAppearance.group();
+    var countryGroup = careerNationality.group();
+    var furthestReached = bestRecord.group();
 
     // Grouping the data - sum
 
     // Charts
     var careerRecords = dc.dataTable("#careerRecords");
+    // var debutYear = dc.rowChart("#debutYear");
+    var countryCareer = dc.pieChart("#countryCareer");
+    var stageReached = dc.pieChart("#stageReached");
 
     careerRecords
       .dimension(playerCareer)
@@ -73,6 +90,31 @@ function makeGraphs(error, crucible_results) {
             return d.frames_lost;
         }
       ])
+
+     // debutYear
+     //  .ordinalColors(["#996600"])
+     //  .width(250)
+     //  .height(750)
+     //  .dimension(firstAppearance) 
+     //  .group(crucibleDebut);
+
+     countryCareer
+      .height(200)
+      .radius(100)
+      .innerRadius(20)
+      .dimension(careerNationality) 
+      .group(countryGroup)
+      .colors(blockSlices)
+      .ordering(function(d) { return -d.value; });
+
+     stageReached
+      .height(200)
+      .radius(100)
+      .innerRadius(20)
+      .dimension(bestRecord) 
+      .group(furthestReached)
+      .colors(shadeSlices)
+      .ordering(function(d) { return d.best_performance; });
 
     dc.renderAll();
 
