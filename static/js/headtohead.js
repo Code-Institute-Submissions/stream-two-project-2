96367bb9c070c77373850795193615480fbe74dc
Andrew Stead <andrew@andrewstead.co.uk>
2018-02-13 15:44:37 +0000

    Tidied up code for H2H content toggle

--- static/js/headtohead.js
+++ static/js/headtohead.js
@@ -4,4 +4,4 @@ function displayH2H() {
 }
 
 // document.getElementById("player1Select").onchange = displayH2H;
-document.getElementById("player2Select").onchange = displayH2H;
+(document.getElementById("player1Select").onchange && document.getElementById("player2Select").onchange) = displayH2H;
